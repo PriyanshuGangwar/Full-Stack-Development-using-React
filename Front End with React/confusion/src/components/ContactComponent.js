6d47c306e4f70e2c9ba5a3e567d1b9d1425541bc
Priyanshu Gangwar <priyanshugangwar02@gmail.com>
2020-07-09 19:57:21 +0530

    Fetch

--- Front End with React/confusion/src/components/ContactComponent.js
+++ Front End with React/confusion/src/components/ContactComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 function Contact(props) {
     return(
--- Front End with React/confusion/src/components/dishcomponent.js
+++ Front End with React/confusion/src/components/dishcomponent.js
@@ -4,7 +4,7 @@ import { Breadcrumb, BreadcrumbItem,Card,CardBody,CardText,CardTitle,CardImg,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len); 
@@ -142,7 +142,7 @@ function RenderDish({dish}) {
         //console.log(dish.dish)
             return(
                 <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
